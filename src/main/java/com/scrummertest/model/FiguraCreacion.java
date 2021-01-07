a0b8eb55f67e8905932470fb32677b8656a83baf
alda1225 <aldairguzman1225@gmail.com>
2021-01-07 13:05:55 -0400

    ultimo cambio

--- src/main/java/com/scrummertest/model/FiguraCreacion.java
+++ src/main/java/com/scrummertest/model/FiguraCreacion.java
@@ -1,15 +1,17 @@
 package com.scrummertest.model;
 
 public class FiguraCreacion {
-	
-	public Figura creaFigura (Float superficie, Float base, Float alto, Float diametro, String tipoFigura) {
-		if (tipoFigura.equalsIgnoreCase("cuadrado")) {
-			return new Cuadrado(superficie, base, tipoFigura);
-		} else if (tipoFigura.equalsIgnoreCase("triangulo")) {
-			return new Triangulo(superficie, base, alto, tipoFigura);
-		} else if (tipoFigura.equalsIgnoreCase("circulo")) {
-			return new Circulo(superficie, diametro, tipoFigura);
-		}	
-		return null;
+
+    public Figura creaFigura(Float superficie, Float base, Float alto, Float diametro, String tipoFigura) {
+	if (tipoFigura.equalsIgnoreCase("cuadrado")) {
+	    return new Cuadrado(superficie, base, tipoFigura);
+	} else if (tipoFigura.equalsIgnoreCase("triangulo")) {
+	    return new Triangulo(superficie, base, alto, tipoFigura);
+	} else if (tipoFigura.equalsIgnoreCase("circulo")) {
+	    return new Circulo(superficie, diametro, tipoFigura);
 	}
+
+	System.out.println(tipoFigura+" => (no es valido para tipo de figura)");
+	return null;
+    }
 }
--- src/main/java/com/scrummertest/model/Main.java
+++ src/main/java/com/scrummertest/model/Main.java
@@ -6,22 +6,16 @@ public class Main {
 	FiguraCreacion factory = new FiguraCreacion();
 	Figura cuadrado = factory.creaFigura(12f, 0f, 8f, 9F, "cuadrado");
 	Figura triangulo = factory.creaFigura(1F, 2F, 4F, 3F, "triangulo");
-	Figura circulo = factory.creaFigura(1F, 2F, 3F, 9F, "circulo");
+	Figura circulo = factory.creaFigura(1F, 2F, 3F, 9F, "s");
 
 	if (cuadrado != null) {
 	    System.out.println(cuadrado.toString());
-	}else{
-	    System.out.println("tipo no valido");
 	}
 	if (triangulo != null) {
 	    System.out.println(triangulo.toString());
-	}else{
-	    System.out.println("tipo no valido");
 	}
 	if (circulo != null) {
 	    System.out.println(circulo.toString());
-	}else{
-	    System.out.println("tipo no valido");
 	}
 
     }
